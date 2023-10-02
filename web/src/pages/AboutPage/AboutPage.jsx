2e7030c110ca08ce7aecd9ab89ad61e6a96e91b8
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-02 16:06:25 +1100

    First lesson done

--- web/src/pages/AboutPage/AboutPage.jsx
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -6,19 +5,7 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Link to={routes.home()}>Home</Link>
-      </main>
+      <></>
     </>
   )
 }
